f4bdddca0998b3fbbf82e4ec39856bf4488ba7ff
vano <vano.atabegashvili@gmail.com>
2015-03-10 12:19:50 +0400

    asdf

--- src/main/java/ge/softgen/loanexpert/spring/AccessInterceptor.java
+++ src/main/java/ge/softgen/loanexpert/spring/AccessInterceptor.java
@@ -28,7 +28,8 @@ public class AccessInterceptor extends HandlerInterceptorAdapter {
 
 		if (ApplicationConfig.isDevMod()) {
 			response.setHeader("Access-Control-Allow-Origin", "*");
-			response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+			response.setHeader("Access-Control-Allow-Methods", "*");
+			response.setHeader("Access-Control-Allow-Credentials", "true");
 			response.setHeader("Access-Control-Max-Age", "3600");
 			response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
 		}
