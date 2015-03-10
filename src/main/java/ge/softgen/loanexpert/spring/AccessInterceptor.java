6b75548ac448390aaabf915d2d23e89d30bc6d30
bacho <bacho.fifia@gmail.com>
2015-03-10 11:24:46 +0400

    fixed Access-Control-Allow-Origin bug on dev mode

--- src/main/java/ge/softgen/loanexpert/spring/AccessInterceptor.java
+++ src/main/java/ge/softgen/loanexpert/spring/AccessInterceptor.java
@@ -26,18 +26,19 @@ public class AccessInterceptor extends HandlerInterceptorAdapter {
 		Class<?> cls = method.getBeanType();
 		Anonymous anonymous = method.getMethodAnnotation(Anonymous.class);
 
+		if (ApplicationConfig.isDevMod()) {
+			response.setHeader("Access-Control-Allow-Origin", "*");
+			response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+			response.setHeader("Access-Control-Max-Age", "3600");
+			response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+		}
+
 		if (anonymous != null)
 			return super.preHandle(request, response, handler);
 
 		SecUser user = checkAuthorization(response);
 		if (user == null || !checkAccesses(cls, request, response, method, user)) {
 			return false;
-		}
-		if (ApplicationConfig.isDevMod()) {
-			response.setHeader("Access-Control-Allow-Origin", "*");
-			response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
-			response.setHeader("Access-Control-Max-Age", "3600");
-			response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
 		}
 		return super.preHandle(request, response, handler);
 	}
