961150196d0e856203256fee34cc87aa60fe10b8
vano <vano.atabegashvili@gmail.com>
2015-03-14 02:16:52 +0400

    rollback

--- src/main/webapp/app/view/customers/CustomerForm.js
+++ src/main/webapp/app/view/customers/CustomerForm.js
@@ -13,15 +13,85 @@ Ext.define('LE.view.customers.CustomerForm', {
         cfg = cfg || {};
         var me = this;
 
-        var individualsTab = Ext.create('LE.view.customers.IndividualsTab');
-        var legalEntitiesTab = Ext.create('LE.view.customers.LegalEntitiesTab');
+        var typeStore = Ext.create('Ext.data.Store', {
+            fields: ['id', 'name'],
+            data: [
+                { id: 1, name: '1'},
+                { id: 2, name: '2'}
+            ]
+        });
+
+        var statusStore = Ext.create('Ext.data.Store', {
+            fields: ['id', 'name'],
+            data: [
+                { id: 1, name: 'VIP კლიენტი'},
+                { id: 2, name: 'შავი სია'},
+                { id: 3, name: 'ნორმალური'},
+                { id: 4, name: 'ახალი'},
+                { id: 5, name: 'სხვა'}
+            ]
+        });
+
+
+        var fieldset1 = Ext.create('Ext.form.FieldSet', {
+            border: false,
+            defaults: {
+                xtype: 'textfield'
+            },
+            items: [{
+                xtype: 'combo',
+                fieldLabel: loc.type,
+                name: 'type',
+                store: typeStore,
+                displayField: 'name',
+                valueField: 'id',
+                queryMode: 'local',
+                editable: false
+            }, {
+                fieldLabel: loc.customers.personalNo,
+                name: 'personalNo',
+                vtype: 'personalno'
+            }, {
+                xtype: 'combo',
+                fieldLabel: loc.status,
+                name: 'status',
+                displayField: 'name',
+                valueField: 'id',
+                queryMode: 'local',
+                editable: false,
+                store: statusStore
+            }]
+        });
+
+        var fieldset2 = Ext.create('Ext.form.FieldSet', {
+            border: false,
+            defaults: {
+                xtype: 'textfield'
+            },
+            items: [{
+                fieldLabel: loc.customers.firstName,
+                name: 'firstName',
+                vtype: 'name'
+            }, {
+                fieldLabel: loc.customers.lastName,
+                name: 'lastName',
+                vtype: 'name'
+            }, {
+                xtype: 'datefield',
+                fieldLabel: loc.customers.birthDate,
+                name: 'birthDate',
+                vtype: 'birthdate'
+            }]
+        });
 
         var extraFieldset = Ext.create("LE.view.customers.ExtraFieldset");
 
         me.items = [{
-            xtype: 'tabpanel',
+            xtype: 'panel',
             border: false,
-            items: [ individualsTab, legalEntitiesTab ]
+            layout: 'hbox',
+            bodyPadding: 15,
+            items: [ fieldset1, fieldset2 ]
         }, extraFieldset];
 
         me.buttons = [{
